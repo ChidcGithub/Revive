7ac258e6392621983b54d89d90b46e7e60329aba
ChidcGithub <chidc@example.com>
2026-03-18 13:34:44 +0800

    v0.1.1 Plain: Fix gradlew permission in Actions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,8 @@ plugins {
 }
 
 // Version configuration
-val appVersionName: String = System.getenv("APP_VERSION_NAME") ?: "1.0.0"
+val appVersionName: String = System.getenv("APP_VERSION_NAME") ?: "0.1.1"
+val versionCodename: String = "Plain"
 val buildNumber: String = System.getenv("GITHUB_RUN_NUMBER") ?: "local"
 val gitSha: String = System.getenv("GIT_SHA") ?: "local"
 val buildType: String = System.getenv("BUILD_TYPE") ?: "r" // "r" for release, "b" for beta
