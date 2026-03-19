92ca66796288e7fe02e44cf1039564cc23021ab4
ChidcGithub <chidc@example.com>
2026-03-19 13:21:32 +0800

    Fix Hilt duplicate binding conflicts for MusicNotificationManager and MusicRepository

--- app/src/main/java/com/music/revive/di/RepositoryModule.kt
+++ app/src/main/java/com/music/revive/di/RepositoryModule.kt
@@ -1,28 +1,11 @@
 package com.music.revive.di
 
-import com.music.revive.data.datasource.MediaStoreDataSource
-import com.music.revive.data.local.dao.FavoriteDao
-import com.music.revive.data.local.dao.PlaylistDao
-import com.music.revive.data.local.dao.RecentSongDao
-import com.music.revive.data.repository.MusicRepository
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object RepositoryModule {
-
-    @Provides
-    @Singleton
-    fun provideMusicRepository(
-        mediaStoreDataSource: MediaStoreDataSource,
-        playlistDao: PlaylistDao,
-        favoriteDao: FavoriteDao,
-        recentSongDao: RecentSongDao
-    ): MusicRepository {
-        return MusicRepository(mediaStoreDataSource, playlistDao, favoriteDao, recentSongDao)
-    }
+    // MusicRepository and MediaStoreDataSource are provided via @Inject constructor
 }
--- app/src/main/java/com/music/revive/di/ServiceModule.kt
+++ app/src/main/java/com/music/revive/di/ServiceModule.kt
@@ -1,23 +1,11 @@
 package com.music.revive.di
 
-import android.content.Context
-import com.music.revive.service.MusicNotificationManager
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
-import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object ServiceModule {
-
-    @Provides
-    @Singleton
-    fun provideMusicNotificationManager(
-        @ApplicationContext context: Context
-    ): MusicNotificationManager {
-        return MusicNotificationManager(context)
-    }
+    // MusicPlayer and MusicNotificationManager are provided via @Inject constructor
 }
